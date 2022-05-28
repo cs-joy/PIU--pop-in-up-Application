7f2b3c4f40bf2e2771f412d57fa34fe6e0e3c1f4
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-05-28 08:19:43 +0600

    Update log-process.php

--- pk/login/log-process.php
+++ pk/login/log-process.php
@@ -1,3 +1,13 @@
 <?php
 
-echo "from log-process!";
+require_once('../config.php');
+
+$query = "SELECT * FROM piu_users";
+$stmtselect = $con->prepare($query);
+$result = $stmtselect->execute([]);
+
+if($result) {
+    echo "Success";
+} else {
+    echo "Error!";
+}
