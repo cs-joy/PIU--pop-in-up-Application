1321dc34cc97d35d363becb7e16ea9077920cb2e
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-05-28 20:26:36 +0600

    Update login.php

--- pk/pTr/login.php
+++ pk/pTr/login.php
@@ -16,7 +16,7 @@ if(isset($_SESSION['userlogin'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
+    <title>Login Page | HIP</title>
 </head>
 
 <body>
